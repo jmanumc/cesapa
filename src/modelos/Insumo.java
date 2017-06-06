6f037bf5681f867d300aae479c2f4b08f7fcfe77
c-salvador-gs <salvador_csgs1997@hotmail.com>
2017-06-06 17:39:05 -0500

    Creado el modelo Insumo.

---
+++ src/modelos/Insumo.java
@@ -0,0 +1,58 @@
+package modelos;
+
+/**
+ *
+ * @author Otro
+ */
+public class Insumo {
+    private Integer id;
+    private int renglon;
+    private String clave;
+    private String descripcion;
+    private String unidadMedida;
+    private String parametro;
+    
+    public Integer getId() {
+        return this.id;
+    }
+    
+    public int getRenglon() {
+        return this.renglon;
+    }
+    
+    public String getDescription() {
+        return this.descripcion;
+    }
+    
+    public String getUnidadMedida() {
+        return this.unidadMedida;
+    }
+    
+    public String getParametro() {
+        return this.parametro;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public void setRenglon(int renglon) {
+        this.renglon = renglon;
+    }
+    
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+    
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    public void setUnidadMedida(String unidadMedida) {
+        this.unidadMedida = unidadMedida;
+    }
+    
+    public void setParametro(String parametro) {
+        this.parametro = parametro;
+    }
+}
