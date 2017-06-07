05cd2e0b6336d978a7e59f6ff162d11378cbbffb
jmanuelmc <j-manuel_mtz23@hotmail.com>
2017-06-06 23:32:53 -0500

    Creado el controlador para los insumos.

---
+++ src/controladores/InsumosControlador.java
@@ -0,0 +1,33 @@
+package controladores;
+
+import java.sql.SQLException;
+import java.util.List;
+import modelos.Insumo;
+import servicios.Conexion;
+import servicios.InsumoServicio;
+
+/**
+ * 
+ * @author Juan Manuel Mc <j-manuel_mtz23 at hotmail.com>
+ */
+public class InsumosControlador {
+    private InsumoServicio insumoServicio;
+    
+    public InsumosControlador() {
+        this.insumoServicio = new InsumoServicio();
+    }
+    
+    public void abrirConexion() throws SQLException, ClassNotFoundException {
+        try {
+            Conexion.obtener();
+        } catch (SQLException ex) {
+            throw new SQLException(ex);
+        } catch (ClassNotFoundException ex) {
+            throw new ClassNotFoundException(ex.getMessage());
+        }
+    }
+    
+    public List<Insumo> index() throws SQLException {
+       return insumoServicio.obtenerTodas(Conexion.conexion);
+    }
+}
